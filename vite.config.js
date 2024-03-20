5dd7cbd87f7a0305e60aecf124161f2dc3e5d935
Jinmoyee Thakuria <jinmoyee99@gmail.com>
2024-03-20 20:45:41 +0530

    1

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:1000",
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
   base: "/Framer-Portfolio-Project/",
 });
